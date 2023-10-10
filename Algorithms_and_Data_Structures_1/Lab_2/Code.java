f19ed17e3ffc9ae6cfc7c2253be51c99c90d5f51
odyess3 <odyess3@gmail.com>
2023-10-10 22:37:49 +0100

    Update Code.java

--- Algorithms_and_Data_Structures_1/Lab_2/Code.java
+++ Algorithms_and_Data_Structures_1/Lab_2/Code.java
@@ -13,13 +13,53 @@ public class Code
         int length = String.valueOf(Snum).length();
 
         int Join = 0;
+        int Total = 0;
+        
 
-        for (int i = length; i>=0; i=i-2)
+        for (int i = length; i>0; i=i-2)
         {
             //System.out.println(i);
 
-            System.out.println(Snum.substring(i-1);
+            //System.out.println(Snum.substring(i-1,i));
+
+            Total += Integer.valueOf(Snum.substring(i-1,i));
+        }
+       //System.out.println(Total);
+
+       int two= 0;
+       int finf =0;
+       int Total2=0;
+
+        for (int j = length-1; j>0; j=j-2)
+        {
+
+            two= Integer.valueOf(Snum.substring(j-1,j))*2;
+
+            if(two>=9)
+            {
+                two=two-9;
+                finf =+ two;
+            }
+
+            else
+            {
+                finf =+ two;
+            }
+
+           Total2= Total2+finf;
 
         }
+
+        //System.out.print(Total2+Total);
+        if((Total2+Total)%10==0)
+        {
+             System.out.print("VALID");
+        }
+        
+        else
+        {
+            System.out.print("INVALID");
+        }
+        
     }
 }
